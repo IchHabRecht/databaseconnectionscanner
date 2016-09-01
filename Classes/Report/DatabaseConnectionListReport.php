9b51ac076e547d1c05fc901edba1bd181a013d38
Nicole Cordes <typo3@cordes.co>
2016-09-01 11:04:57 +0200

    [FEATURE] Add extension and file list

--- Classes/Report/DatabaseConnectionListReport.php
+++ Classes/Report/DatabaseConnectionListReport.php
@@ -25,6 +25,13 @@ namespace IchHabRecht\Databaseconnectionscanner\Report;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
+use Symfony\Component\Finder\Finder;
+use TYPO3\CMS\Core\Core\Bootstrap;
+use TYPO3\CMS\Core\Database\DatabaseConnection;
+use TYPO3\CMS\Core\Package\Package;
+use TYPO3\CMS\Core\Package\PackageManager;
+use TYPO3\CMS\Core\Utility\PathUtility;
+use TYPO3\CMS\Dbal\Database\DatabaseConnection as DbalDatabaseConnection;
 use TYPO3\CMS\Reports\ReportInterface;
 
 class DatabaseConnectionListReport implements ReportInterface
@@ -36,6 +43,62 @@ class DatabaseConnectionListReport implements ReportInterface
      */
     public function getReport()
     {
-        return '';
+        $packages = $this->getSortedPackages();
+
+        $content = '';
+        /** @var Package $package */
+        foreach ($packages as $package) {
+            $packagePath = rtrim($package->getPackagePath(), '/');
+            $finder = new Finder();
+            $finder
+                ->files()
+                ->name('*.php')
+                ->in($packagePath)
+                ->contains('$GLOBALS[\'TYPO3_DB\']')
+                ->contains(DatabaseConnection::class)
+                ->contains(DbalDatabaseConnection::class);
+
+            $files = iterator_to_array($finder);
+            if (empty($files)) {
+                continue;
+            }
+
+            $content .= '<h2>EXT:' . $package->getPackageKey() . '</h2>';
+            $content .= '<table class="table table-striped table-hover">';
+            $content .= '<tbody>';
+            foreach ($files as $file) {
+                $content .= '
+                    <tr>
+                        <td class="col-xs-12">' . PathUtility::getRelativePath($packagePath, $file->getPath()) . $file->getFileName() . '</td>
+                    </tr>';
+            }
+            $content .= '</tbody>';
+            $content .= '</table>';
+        }
+
+        return $content;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getSortedPackages()
+    {
+        $packageManager = Bootstrap::getInstance()->getEarlyInstance(PackageManager::class);
+        $packages = $packageManager->getAvailablePackages();
+
+        usort($packages, function (Package $a, Package $b) {
+            $isASysExt = 'typo3-cms-framework' === $a->getValueFromComposerManifest('type');
+            $isBSysExt = 'typo3-cms-framework' === $b->getValueFromComposerManifest('type');
+            if ($isASysExt && $isBSysExt || !$isASysExt && !$isBSysExt) {
+                return strcmp($a->getPackageKey(), $b->getPackageKey());
+            } elseif ($isASysExt) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+
+        return $packages;
     }
 }
