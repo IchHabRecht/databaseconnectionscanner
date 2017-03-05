0d09ba5adb85f71077264f0e1934021031f39981
Nicole Cordes <typo3@cordes.co>
2017-03-05 23:46:58 +0100

    [RELEASE] Release of databaseconnectionscanner 0.1.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "databaseconnectionscanner".
  *
- * Auto generated 31-12-2016 14:45
+ * Auto generated 05-03-2017 23:46
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 1,
-  'version' => '0.1.2',
+  'version' => '0.1.3',
   'constraints' => 
   array (
     'depends' => 
@@ -36,6 +36,6 @@ $EM_CONF[$_EXTKEY] = array (
     array (
     ),
   ),
-  '_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"65e8";s:13:"composer.json";s:4:"bd34";s:12:"ext_icon.png";s:4:"c236";s:14:"ext_tables.php";s:4:"df2b";s:47:"Classes/Report/DatabaseConnectionListReport.php";s:4:"1780";s:46:"Resources/Private/Language/de.locallang_be.xlf";s:4:"fe67";s:43:"Resources/Private/Language/locallang_be.xlf";s:4:"db7d";}',
+  '_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"dc02";s:13:"composer.json";s:4:"e91e";s:12:"ext_icon.png";s:4:"c236";s:14:"ext_tables.php";s:4:"df2b";s:47:"Classes/Report/DatabaseConnectionListReport.php";s:4:"6fde";s:46:"Resources/Private/Language/de.locallang_be.xlf";s:4:"fe67";s:43:"Resources/Private/Language/locallang_be.xlf";s:4:"db7d";}',
 );
 
